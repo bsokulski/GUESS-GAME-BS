4fc51d2e6858d2854566cbaccf479eccca4a9ab1
bsokulski <bsokulski@stkentigernacademy.org.uk>
2016-06-23 16:38:55 +0100

    guess game

---
+++ GUESS.java
@@ -0,0 +1,67 @@
+
+import javax.swing.*;
+public class GUESS
+{
+    private String playerName;
+    
+    public GUESS()
+    {
+      playerName = ("");
+    }
+    
+    public void Display_opening_message()
+    {
+       System.out.println("welcome to the Guessing game");
+    }
+    
+    public void Play_Game()
+    {
+       //3.1    Display Menu
+         //3.1
+       //3.2    If option is equal to play then
+         //3.2
+       //3.3    Enter player name
+        // 3.3
+       //3.4    Generate random number
+        // 3.4
+              //3.4.1    Computer generates a number between 1 and 50
+           //     3.4.1
+       ///3.5    While found equals false do
+        // 3.5
+       //3.6    enter lucky guess from player
+        // 3.6
+       //3.7    if guess equals random number then
+        // 3.7
+       //3.8    set found to true
+       //  3.8
+       //3.9    else if guess is less than random number then
+        // 3.9
+       //3.10   display a less than message
+        // 3.10
+       //3.11   else
+        // 3.11
+       //3.12   display a more than message
+        // 3.12
+       //3.13   end if
+        // 3.13
+       //3.14   end while
+        // 3.14
+       //3.15   Display results
+        // 3.15
+       //3.16   else
+        // 3.16
+       //3.17   quit game
+         //3.17
+       //3.18   end if
+        // 3.18
+
+    }
+    
+    public void Display_closing_message()
+    {
+       ;
+    }
+
+   
+    
+}
